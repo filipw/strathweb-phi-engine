87a5050ab758ebda54164986bc4ad199b98622c0
filipw <filip@strathweb.com>
2024-07-05 17:11:18 +0200

    use regular Phi3 architecture (not llama)

--- strathweb-phi-engine/src/lib.rs
+++ strathweb-phi-engine/src/lib.rs
@@ -3,8 +3,8 @@ uniffi::include_scaffolding!("strathweb-phi-engine");
 use anyhow::{Error as E, Result};
 use candle_core::quantized::gguf_file;
 use candle_core::{Device, Tensor};
-use candle_transformers::generation::LogitsProcessor;
-use candle_transformers::models::quantized_llama::ModelWeights as QPhi3;
+use candle_transformers::generation::{LogitsProcessor, Sampling};
+use candle_transformers::models::quantized_phi3::ModelWeights as Phi3;
 use hf_hub::api::sync::ApiBuilder;
 use hf_hub::Repo;
 use std::collections::VecDeque;
@@ -16,7 +16,7 @@ use thiserror::Error;
 use tokenizers::Tokenizer;
 
 struct PhiEngine {
-    pub model: QPhi3,
+    pub model: Phi3,
     pub tokenizer: Tokenizer,
     pub device: Device,
     pub history: Mutex<VecDeque<String>>,
@@ -27,8 +27,9 @@ struct PhiEngine {
 #[derive(Debug, Clone)]
 pub struct InferenceOptions {
     pub token_count: u16,
-    pub temperature: Option<f64>,
+    pub temperature: f64,
     pub top_p: Option<f64>,
+    pub top_k: Option<u64>,
     pub repeat_penalty: f32,
     pub repeat_last_n: u16,
     pub seed: u64,
@@ -49,6 +50,7 @@ pub struct EngineOptions {
     pub tokenizer_repo: Option<String>,
     pub model_file_name: Option<String>,
     pub model_revision: Option<String>,
+    pub use_flash_attention: bool,
     pub system_instruction: Option<String>,
 }
 
@@ -137,11 +139,11 @@ impl PhiEngine {
         let mut file = File::open(&model_path).map_err(|e| PhiError::InitalizationError {
             error_text: e.to_string(),
         })?;
-        let model =
+        let model_content =
             gguf_file::Content::read(&mut file).map_err(|e| PhiError::InitalizationError {
                 error_text: e.to_string(),
             })?;
-        let model = QPhi3::from_gguf(model, &mut file, &device).map_err(|e| {
+        let model = Phi3::from_gguf(engine_options.use_flash_attention, model_content, &mut file, &device).map_err(|e| {
             PhiError::InitalizationError {
                 error_text: e.to_string(),
             }
@@ -229,7 +231,7 @@ impl PhiEngine {
 }
 
 struct TextGeneration {
-    model: QPhi3,
+    model: Phi3,
     device: Device,
     tokenizer: Tokenizer,
     logits_processor: LogitsProcessor,
@@ -239,17 +241,26 @@ struct TextGeneration {
 
 impl TextGeneration {
     fn new(
-        model: &QPhi3,
+        model: &Phi3,
         tokenizer: Tokenizer,
         inference_options: &InferenceOptions,
         device: &Device,
         event_handler: Arc<BoxedPhiEventHandler>,
     ) -> Self {
-        let logits_processor = LogitsProcessor::new(
-            inference_options.seed,
-            inference_options.temperature,
-            inference_options.top_p,
-        );
+        let logits_processor = {
+            let temperature = inference_options.temperature;
+            let sampling = if temperature <= 0. {
+                Sampling::ArgMax
+            } else {
+                match (inference_options.top_k, inference_options.top_p) {
+                    (None, None) => Sampling::All { temperature },
+                    (Some(k), None) => Sampling::TopK { k: usize::try_from(k).unwrap_or(0), temperature },
+                    (None, Some(p)) => Sampling::TopP { p, temperature },
+                    (Some(k), Some(p)) => Sampling::TopKThenTopP { k: usize::try_from(k).unwrap_or(0), p, temperature },
+                }
+            };
+            LogitsProcessor::from_sampling(inference_options.seed, sampling)
+        };
         Self {
             model: model.clone(),
             tokenizer,
@@ -383,7 +394,6 @@ impl TokenOutputStream {
         }
     }
 
-    // https://github.com/huggingface/text-generation-inference/blob/5ba53d44a18983a4de32d122f4cb46f4a17d9ef6/server/text_generation_server/models/model.py#L68
     pub fn next_token(&mut self, token: u32) -> Result<Option<String>> {
         let prev_text = if self.tokens.is_empty() {
             String::new()
@@ -393,25 +403,11 @@ impl TokenOutputStream {
         };
         self.tokens.push(token);
         let text = self.decode(&self.tokens[self.prev_index..])?;
-
-        if text.len() > prev_text.len() {
-            match text
-                .chars()
-                .last()
-                .ok_or_else(|| anyhow::Error::msg("No last character"))
-            {
-                Ok(last_char) => {
-                    if last_char.is_ascii() {
-                        let text = text.split_at(prev_text.len());
-                        self.prev_index = self.current_index;
-                        self.current_index = self.tokens.len();
-                        Ok(Some(text.1.to_string()))
-                    } else {
-                        Ok(None)
-                    }
-                }
-                Err(e) => Err(e),
-            }
+        if text.len() > prev_text.len() && text.chars().last().unwrap().is_alphanumeric() {
+            let text = text.split_at(prev_text.len());
+            self.prev_index = self.current_index;
+            self.current_index = self.tokens.len();
+            Ok(Some(text.1.to_string()))
         } else {
             Ok(None)
         }
