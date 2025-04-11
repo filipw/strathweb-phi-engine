9fa7d62cc75d94f7be070b811e99d1b8852ca765
filipw <filip@strathweb.com>
2025-04-11 18:14:02 +0200

    updated to uniffi 0.28.3

--- strathweb-phi-engine/build.rs
+++ strathweb-phi-engine/build.rs
@@ -1,25 +1,16 @@
 use std::process::Command;
-use uniffi_bindgen::{bindings::TargetLanguage, generate_bindings};
+use uniffi_bindgen::{bindings::{KotlinBindingGenerator, PythonBindingGenerator, SwiftBindingGenerator}, generate_bindings};
 
 fn main() {
     let udl_file = "./src/strathweb-phi-engine.udl";
     let out_dir = "./bindings";
 
     uniffi::generate_scaffolding(udl_file).unwrap();
-    generate_bindings(
-        udl_file.into(),
-        None,
-        vec![
-            TargetLanguage::Swift,
-            TargetLanguage::Kotlin,
-            TargetLanguage::Python,
-        ],
-        Some(out_dir.into()),
-        None,
-        None,
-        false,
-    )
-    .unwrap(); 
+
+    generate_bindings(udl_file.into(), None, PythonBindingGenerator, Some(out_dir.into()), None, None, false).unwrap_or_else(|e| eprintln!("Failed to generate Python bindings: {}", e));
+    generate_bindings(udl_file.into(), None, KotlinBindingGenerator, Some(out_dir.into()), None, None, false).unwrap_or_else(|e| eprintln!("Failed to generate Kotlin bindings: {}", e));
+    generate_bindings(udl_file.into(), None, SwiftBindingGenerator, Some(out_dir.into()), None, None, false).unwrap_or_else(|e| eprintln!("Failed to generate Swift bindings: {}", e));
+
 
     let status = Command::new("uniffi-bindgen-cs")
         .arg("--out-dir")
